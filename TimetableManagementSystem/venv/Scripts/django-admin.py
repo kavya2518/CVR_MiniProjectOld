2b55feae7e51f6aa3d75cc8964c92905889a3837
Kavya <dubbakakavya@gmail.com>
2019-11-06 22:07:47 +0530

    venv changed

---
+++ TimetableManagementSystem/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\CVR\CVR_MiniProject\TimetableManagementSystem\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
